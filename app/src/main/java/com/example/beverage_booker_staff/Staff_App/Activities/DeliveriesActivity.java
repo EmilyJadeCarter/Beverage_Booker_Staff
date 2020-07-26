7a7f1185888608f575198c19452dde2b3d6700f8
bcurby <41361395+bcurby@users.noreply.github.com>
2020-07-26 10:21:45 +1000

    006 - Delivery - Recyclerview and adapter displaying deliveries

--- app/src/main/java/com/example/beverage_booker_staff/Staff_App/Activities/DeliveriesActivity.java
+++ app/src/main/java/com/example/beverage_booker_staff/Staff_App/Activities/DeliveriesActivity.java
@@ -1,16 +1,124 @@
 package com.example.beverage_booker_staff.Staff_App.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.beverage_booker_staff.R;
+import com.example.beverage_booker_staff.Staff_App.API.RetrofitClient;
+import com.example.beverage_booker_staff.Staff_App.Adaptors.DeliveriesAdapter;
+import com.example.beverage_booker_staff.Staff_App.Models.Deliveries;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DeliveriesActivity extends AppCompatActivity {
+
+    private ArrayList<Deliveries> mDeliveries;
+    private DeliveriesAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+
+    private Button deliveredButton;
+
+    private Timer deliveryTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deliveries);
+
+        mRecyclerView = findViewById(R.id.recyclerView3);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mDeliveries = new ArrayList<>();
+        mAdapter = new DeliveriesAdapter(mDeliveries);
+        mRecyclerView.setAdapter(mAdapter);
+
+        deliveryTimer = new Timer();
+        deliveryTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                mDeliveries.clear();
+
+                //Delivered Button
+                mAdapter.setOnItemClickListener(new DeliveriesAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClickListener(int position) {
+
+                        int userID = mDeliveries.get(position).getUserID();
+                        System.out.println(userID);
+                        int cartID = mDeliveries.get(position).getCartID();
+                        System.out.println(cartID);
+                        markOrderDelivered(userID, cartID);
+                    }
+                });
+
+
+                Call<List<Deliveries>> call = RetrofitClient
+                        .getInstance()
+                        .getApi()
+                        .getDeliveriesList();
+
+                call.enqueue(new Callback<List<Deliveries>>() {
+
+                    @Override
+                    public void onResponse(Call<List<Deliveries>> call, Response<List<Deliveries>> response) {
+                        if (response.code() == 200) {
+                            for (int i = 0; i < response.body().size(); i++) {
+                                mDeliveries.add(response.body().get(i));
+                            }
+
+                            mAdapter.notifyDataSetChanged();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Deliveries>> call, Throwable t) {
+                        Toast.makeText(DeliveriesActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }, 0, 500);
+    }
+
+    private void markOrderDelivered(int userID, int cartID) {
+
+        Call<ResponseBody> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .markDelivered(userID, cartID);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                if (response.code() == 201) {
+                    Toast.makeText(DeliveriesActivity.this, "Order Delivered", Toast.LENGTH_LONG).show();
+
+                } else if (response.code() == 422) {
+                    Toast.makeText(DeliveriesActivity.this, "Marking order delivered failed",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(DeliveriesActivity.this, t.getMessage(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/beverage_booker_staff/Staff_App/Models/Deliveries.java
@@ -0,0 +1,45 @@
+package com.example.beverage_booker_staff.Staff_App.Models;
+
+public class Deliveries {
+
+    private int userID;
+    private int cartID;
+    private int streetNumber;
+    private String streetName;
+    private int postCode;
+    private String cityTown;
+
+    public Deliveries(int userID, int cartID, int streetNumber, String streetName, int postCode, String cityTown) {
+
+        this.userID = userID;
+        this.cartID = cartID;
+        this.streetNumber = streetNumber;
+        this.streetName = streetName;
+        this.postCode = postCode;
+        this.cityTown = cityTown;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public int getCartID() {
+        return cartID;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public int getPostCode() {
+        return postCode;
+    }
+
+    public String getCityTown() {
+        return cityTown;
+    }
+}
