f30f5a371d155cf037170b984a2df9932489e064
jakedurn <jakeadurnford@gmail.com>
2020-09-12 19:50:40 +1000

    Start of Create Staff User
    The create staff page

---
+++ app/src/main/java/com/example/beverage_booker_staff/Staff_App/Activities/CreateStaffActivity.java
@@ -0,0 +1,15 @@
+package com.example.beverage_booker_staff.Staff_App.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.beverage_booker_staff.R;
+
+import android.os.Bundle;
+
+public class CreateStaffActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_staff);
+    }
+}
