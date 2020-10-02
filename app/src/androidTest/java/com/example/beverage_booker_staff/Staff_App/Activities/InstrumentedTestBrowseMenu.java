e363964ccd43bf6eb61fbef21cae2fca92ea0ed7
jacobk50 <jacobkennedy1@live.co.uk>
2020-10-02 15:43:56 +1000

    Fixed InstraTestBrowseMenu

--- app/src/androidTest/java/com/example/beverage_booker_staff/Staff_App/Activities/InstrumentedTestBrowseMenu.java
+++ app/src/androidTest/java/com/example/beverage_booker_staff/Staff_App/Activities/InstrumentedTestBrowseMenu.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
@@ -32,33 +33,43 @@ public class InstrumentedTestBrowseMenu {
      * From main menu this test click menu button and ensures the screen changed
      * by checking and making sure every category of item is displayed properly
      * in which it checks all components related to banana. he prerequisites for this
-     * test are a staff member exists with the id 1001 and an entry in the items
-     * database exists with a name of "banana", description of "its a banana"
-     * and a price of 12.5.
+     * test are a staff member exists with the id 1001.
      */
     @Test
-    public void ClickOnMenu_AssertMenuOpens() {
+    public void ClickOnMenu_AssertMenuOpens() throws InterruptedException {
+        //MainMenuActivity
         onView(withId(R.id.MenuButton))
                 .perform(click());
+        Thread.sleep(4000);
+
+        //BrowseMenuActivity
         onView(withId(R.id.MenuButton))
                 .check(doesNotExist());
-        onView(withId(R.id.OrdersButton))
-                .check(doesNotExist());
-        onView(withId(R.id.DeliveriesButton))
-                .check(doesNotExist());
-        onView(allOf(ViewMatchers.withId(R.id.itemID), hasSibling(withText("1"))))
-                .check(matches(isDisplayed()));
-        onView(allOf(ViewMatchers.withId(R.id.itemName), hasSibling(withText("banana"))))
-                .check(matches(isDisplayed()));
-        onView(allOf(ViewMatchers.withId(R.id.itemDesc), hasSibling(withText("its a banana"))))
-                .check(matches(isDisplayed()));
-        onView(allOf(ViewMatchers.withId(R.id.itemPrice), hasSibling(withText("$12.50"))))
-                .check(matches(isDisplayed()));
-        onView(allOf(ViewMatchers.withId(R.id.modifyMenuItem), hasSibling(withText("banana"))))
-                .check(matches(isDisplayed()));
-        onView(allOf(ViewMatchers.withId(R.id.deleteMenuItem), hasSibling(withText("banana"))))
-                .check(matches(isDisplayed()));
         onView(withId(R.id.addMenuItem))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void ClickOnMenu_AssertItemsExist() throws InterruptedException {
+        //MainMenuActivity
+        onView(withId(R.id.MenuButton))
+                .perform(click());
+        Thread.sleep(4000);
+
+        //BrowseMenuActivity
+        onView(withId(R.id.MenuButton))
+                .check(doesNotExist());
+        onView(withId(R.id.addMenuItem))
+                .check(matches(isDisplayed()));
+        onView(allOf(ViewMatchers.withId(R.id.itemID), hasSibling(withText("7"))))
+                .check(matches(isDisplayed()));
+        onView(allOf(ViewMatchers.withId(R.id.itemName), hasSibling(withText("Flat White"))))
+                .check(matches(isDisplayed()));
+        onView(allOf(ViewMatchers.withId(R.id.itemPrice), hasSibling(withText("Flat White"))))
+                .check(matches(hasSibling(withText("$3.90"))));
+        onView(allOf(ViewMatchers.withId(R.id.modifyMenuItem), hasSibling(withText("Flat White"))))
+                .check(matches(isDisplayed()));
+        onView(allOf(ViewMatchers.withId(R.id.deleteMenuItem), hasSibling(withText("Flat White"))))
                 .check(matches(isDisplayed()));
     }
 }
